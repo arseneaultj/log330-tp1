0d6fa2bc49130c8f5bccb56a6a436dfb3234faf9
Joel Arseneault <arseneaultjoel@gmail.com>
2018-06-17 18:08:33 -0400

    Update csv reader function to new format

--- main.go
+++ main.go
@@ -18,18 +18,18 @@ func main() {
 
 	values := readCsv(os.Args[1])
 
-	moyenne := getAverage(values)
-	variance := getVariance(values)
-	ecartType := math.Sqrt(variance)
-	fmt.Printf("Moyenne : %v\n", moyenne)
-	fmt.Printf("Variance : %.4f\n", variance)
-	fmt.Printf("Ecart type : %.2f\n", ecartType)
+	//moyenne := getAverage(values)
+	//variance := getVariance(values)
+	//ecartType := math.Sqrt(variance)
+	//fmt.Printf("Moyenne : %v\n", moyenne)
+	//fmt.Printf("Variance : %.4f\n", variance)
+	//fmt.Printf("Ecart type : %.2f\n", ecartType)
 
 }
 
-func readCsv(path string) [] int{
+func readCsv(path string) [] point{
 
-	var values []int
+	var values []point
 
 	file,err := os.Open(path)
 	if err != nil {
@@ -43,8 +43,9 @@ func readCsv(path string) [] int{
 	lines,_ := reader.ReadAll()
 
 	for i := range lines {
-		value,_ := strconv.Atoi(lines[i][0])
-		values = append(values, value)
+		x,_ := strconv.ParseFloat(lines[i][0],64)
+		y,_ := strconv.ParseFloat(lines[i][1],64)
+		values = append(values, point{x,y})
 	}
 
 	return values
